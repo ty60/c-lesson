998cc49283f41f1fae26c472e9a88300d7436658
ty60 <mmttt202@gmail.com>
2022-02-27 19:33:55 +0900

    print hex

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -6,9 +6,26 @@
 */
 .globl _start
 _start:
+    ldr r0, =0x101f1000
     ldr r1,=0xdeadbeaf
     b print_hex
 print_hex:
-    // TODO: implement here
+    // print hex digit
+    ldrb r2, =0xf0000000
+    and r2, r1, r2
+    lsr r2, r2, #28
+    ldr r3, =hex_digits
+    add r3, r2, r3
+    ldrb r4, [r3]
+    strb r4, [r0]
+    // shift left and goto next loop
+    lsl r1, r1, #4
+    cmp r1, #0
+    bne print_hex
+    mov r4, #0xa
+    strb r4, [r0]
 end:
     b end
+
+hex_digits:
+    .asciz "0123456789abcdef"
